3c1aa45ba9ceb9738a9d5996a4955d12f962496c
Булат Каримов <bulatkarimov96@gmail.com>
2019-09-20 13:19:05 +0300

    [add] layuot

--- js/js.js
+++ js/js.js
@@ -1,11 +1,13 @@
-$(function (){
-   $("#btn").click(function(){
-     $("#text")
-           .css("color", "green")
-           .html("Новый текст")
-           .css("background-color", "#333")
+$(document).ready(function(){
+   console.log('Started')
+
+   $('#lin_solve').on('click', function(e){
+      let a = $('#lin_a').val()
+      let b = $('#lin_b').val()
+      let c = $('#lin_c').val()
+
+      console.log(params)
+      
+      console.log('Lin urav')
    });
-    $("#reset").click(function(){
-       location.reload(); 
-    });
 });

142bf4c5f4288757c5d221fb0eeb41a0d906622e
Булат Каримов <bulatkarimov96@gmail.com>
2019-09-22 20:44:28 +0300

    [add] request for API

--- js/js.js
+++ js/js.js
@@ -1,13 +1,67 @@
 $(document).ready(function(){
    console.log('Started')
 
+   // lin solve
    $('#lin_solve').on('click', function(e){
       let a = $('#lin_a').val()
       let b = $('#lin_b').val()
       let c = $('#lin_c').val()
 
-      console.log(params)
+      if($.isNumeric(a) && $.isNumeric(b) && $.isNumeric(c)){
+         console.log(a, b, c)
+
+         a = parseInt(a)
+         b = parseInt(b)
+         c = parseInt(c)
+
+         console.log(a, b, c)
       
-      console.log('Lin urav')
+         $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/api/v1/solutions/solve",
+            data: {"a": a, "b": b, "c": c, "type": "lin"},
+            success: function(data) {
+                alert("X = " + data.result);
+            },
+            error: function(data){
+                alert(data.message);
+            }
+        });
+      }else{
+         alert('Неверные параметры')
+      }
+   });
+
+   // sqr solve
+   $('#sqr_solve').on('click', function(e){
+      let a = $('#sqr_a').val()
+      let b = $('#sqr_b').val()
+      let c = $('#sqr_c').val()
+      let d = $('#sqr_d').val()
+
+      if($.isNumeric(a) && $.isNumeric(b) && $.isNumeric(c) && $.isNumeric(d)){
+         console.log(a, b, c, d)
+
+         a = parseInt(a)
+         b = parseInt(b)
+         c = parseInt(c)
+         d = parseInt(d)
+
+         console.log(a, b, c, d)
+      
+         $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/api/v1/solutions/solve",
+            data: {"a": a, "b": b, "c": c, "d": d, "type": "sqrt"},
+            success: function(data) {
+                alert(data.result);
+            },
+            error: function(data){
+                alert(data.message);
+            }
+        });
+      }else{
+         alert('Неверные параметры')
+      }
    });
 });
